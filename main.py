3e46164311f850ab66a256a62fa56d3354c8a394
HaoZhengZhao <haozheng.zhao@poper.co>
2025-11-21 20:06:33 +0800

    fix: 更新自定义acount和重发发送限制

--- main.py
+++ main.py
@@ -9,15 +9,12 @@ import os
 # 加载 .env 文件
 load_dotenv()  # 默认加载项目根目录的 .env 文件
 
+has_sends_url = {} # 记录已发送通知的新闻链接,避免重复发送，最多存储1000条
+
 dingding_token = os.getenv("dingding_token")
-
-
-binance_accounts = [
-    'binance_announcement',
-    'binance_news',
-    'binancezh',
-    'marktowin',
-]
+binance_accounts_str = os.getenv("binance_accounts", "")
+binance_accounts = binance_accounts_str.split(",") if binance_accounts_str else []
+effective_time = int(os.getenv("effective_time", "5"))
 
 async def binance_run(playwright: Playwright, accounts):
     chromium = playwright.chromium # or "firefox" or "webkit".
@@ -31,7 +28,7 @@ async def binance_run(playwright: Playwright, accounts):
         first_article_url = None
         index = 0
         while not first_article_url:
-            cardText = await page.locator('.feed-layout-main .FeedList .feed-content-text').nth(index).text_content()  # 获取文章内容
+            cardText = await page.locator('.feed-layout-main .FeedList .feed-card').nth(index).all_text_contents()
             if '置顶' not in cardText:
                 first_article_url = await page.locator('.feed-layout-main .FeedList .feed-content-text a').nth(index).get_attribute('href') # 找到第一个非置顶，进入明细页面
             index += 1
@@ -45,12 +42,16 @@ async def binance_run(playwright: Playwright, accounts):
             continue
         mins = timeArr[0]  # 获取分钟数
         ext = timeArr[1]  # 获取时间单位（分钟、小时等）
-        if int(mins) <= 5 and ext.startswith('分钟'):
+        if int(mins) <= effective_time and ext.startswith('分钟'):
             # 3 分钟前的新闻发送通知
             print('准备发送钉钉通知')
             article_text = await page.locator('.feed-layout-main .richtext-container').text_content()
             if not check_keywords(article_text):
                 continue
+            if has_sends_url.get(detail_url):
+                print('该新闻已发送过通知，跳过')
+                continue        
+            has_sends_url[detail_url] = True
             res = send_dingtalk_markdown('bn报警通知: ' + account, article_text)
             print(res)
     await browser.close()
@@ -63,7 +64,6 @@ def check_keywords(article: str):
     """
     keywords = ['活动', '奖励', 'Alpha', '上线', '上市', 'alpha', '空投', '赠送', '福利', '送出', '补贴', '交易大赛', '交易竞赛', '抽取', '抽奖', '赠币', '奖励金', '奖励计划']
     return any(keyword in article for keyword in keywords)
-    
     
 def send_dingtalk_markdown(title, text, is_at_all=True):
     """
@@ -103,6 +103,8 @@ def get_current_time():
 async def main():
     print('Starting Binance news monitoring...')
     while True:
+        if has_sends_url.__len__() > 1000:
+            has_sends_url.clear()  # 清理已发送记录，防止内存占用过高
         print('Checking Binance news...')
         async with async_playwright() as playwright:
             await binance_run(playwright, binance_accounts)
